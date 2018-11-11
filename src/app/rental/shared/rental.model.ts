a439690973bd31fd12749fbb3b830ed590780509
Jeff Snyder <jeffery.s.snyder@gmail.com>
2018-11-10 22:04:36 -0600

    Adds rental lists compenent, adds rental detail component, adds renatl service and renatl model

---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,14 @@
+export class Rental {
+    id: string;
+    title: string;
+    city: string;
+    street: string;
+    category: string;
+    image: string;
+    bedrooms: number;
+    descritpion: string;
+    dailyRate: number;
+    shared: boolean;
+    createdAt: string;
+}
+
